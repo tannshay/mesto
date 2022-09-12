db6ff7dc21be2d2430be3d0ea0e0fbe7b3e09922
tannshay <shaytann831@gmail.com>
2022-09-12 15:33:44 +0300

    4 commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,21 @@ const popupEl = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__form');
 const openEditProfileButton = document.querySelector('.profile__edit');
 const closeEditProfileButton = document.querySelector('.popup__close-button');
-const nameInput = document.querySelector('.popup__text_name');
-const aboutInput = document.querySelector('.popup__text_about');
+const nameInput = document.querySelector('.popup__text_type_name');
+const aboutInput = document.querySelector('.popup__text_type_about');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const togglePopup = function () {
     popupEl.classList.toggle('popup_opened');
 }
 
-openEditProfileButton.addEventListener('click', function () {
+openEditProfileButton.addEventListener('click', function openpopup () {
     togglePopup();
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 })
 
-closeEditProfileButton.addEventListener('click', function () {
+closeEditProfileButton.addEventListener('click', function closepopup () {
     togglePopup();
 })
 
